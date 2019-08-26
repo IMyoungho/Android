23bca5d434448f8925c9798cf384286f765e9a96
정영호 <jung-youngho@jeong-yeonghoui-MacBook-Pro.local>
2019-08-26 16:13:35 +0900

    notice & note

---
+++ Notice_Note/app/src/main/java/com/example/notice/NoticeList.java
@@ -0,0 +1,5 @@
+//package com.example.notice;
+//
+//public class NoticeList {
+//
+//}
---
+++ Notice_Note/app/src/main/java/com/example/notice/Notice_db.java
@@ -0,0 +1,44 @@
+//package com.example.notice;
+//
+//public class Notice_db {
+//    String num;
+//    String title;
+//    String writer;
+//    String date;
+//    public String get_num(){
+//        return num;
+//    }
+//    public void set_num(String num){
+//        this.num=num;
+//    }
+//
+//    public String get_title(){
+//        return title;
+//    }
+//
+//    public void set_title(String title){
+//        this.title=title;
+//    }
+//
+//
+//    public String get_writer(){
+//        return writer;
+//    }
+//
+//    public void set_writer(String writer){
+//        this.writer=writer;
+//    }
+//    public String get_date(){
+//        return date;
+//    }
+//
+//    public void set_date(String date){
+//        this.date=date;
+//    }
+//    public Notice_db(String num, String title, String writer, String date){
+//        this.num=num;
+//        this.title=title;
+//        this.writer=writer;
+//        this.date=date;
+//    }
+//}
